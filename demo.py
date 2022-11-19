e13579d68ac790022d596c54b3f9b0f8ff38718e
Jenit Jain <jenitjai@student.ubc.ca>
2022-11-19 12:37:43 -0800

    Add new positional argument to demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,8 @@
 """This script prints out docopt args.
 Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
 Options:
-<arg1>             Takes any value (this is a required positional argument)
+<arg1>            Takes any value (this is a required positional argument)
+[<arg_new>]       Takes any value (this is an optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 """ 
